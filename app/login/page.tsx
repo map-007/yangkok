b922cb37453af91902d2700b922088cab9bfeb0c
map007 <min.aungpaing007@gmail.com>
2024-11-07 23:26:29 +0630

    add: login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image";
 import style from "./login.module.scss";
+import { FaTelegramPlane } from "react-icons/fa";
+import Link from "next/link";
 const LoginPage = () => {
-  return <div className="flex justify-center items-center">Login page</div>;
+  return (
+    <div className={style.loginPage}>
+      <div className={style.container}>
+        <p>တယ်လီဂရမ်ဖြင့် ဝင်ပါ</p>
+        <Link href="/" className={style.button}>
+          <FaTelegramPlane />
+          <span>Login as Guest</span>
+          <Image
+            src="/vercel.svg"
+            className={style.profileImg}
+            alt="telegram"
+            width={20}
+            height={20}
+          />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const [user] = useState(null);
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [router, user]);
   return <div className="">Hello from Yangkok</div>;
 }
